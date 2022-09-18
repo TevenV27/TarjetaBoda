5cdde7f6490d4b455a70729990c0c73f70b13df6
Steven Victoria <102372427+TevenV27@users.noreply.github.com>
2022-09-18 01:26:09 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1,36 +1,31 @@
 const btn = document.querySelector("#boton");
-const book = document.querySelector("#book");
+const btnBox = document.querySelector("#blur");
+const infor = document.querySelector("#information");
 const pag1 = document.querySelector("#p1");
 const pag2 = document.querySelector("#p2");
 const tar = document.querySelector("#tar");
 const p1 = document.querySelector("#p1");
 const p2 = document.querySelector("#p2");
 
-btn.addEventListener("click", animation);
+btn.addEventListener("click", click);
 
-let base = 1;
-let numTapas = 1;
-let maxima = numTapas + 1;
+function click(){
+  btn.style.display = "none";
+  btnBox.style.display = "none";
+  setTimeout(music,3000);
+  setTimeout(animation,3000);
+}
 
-function animation() {
-    
+function music(){
   let etiquetaAudio = document.createElement("audio");
   etiquetaAudio.setAttribute("src", "music/lava.mp3");
   etiquetaAudio.play();
+}
+function animation() {
 
-  if (base < maxima) {
-    switch (base) {
-      case 1:
-        /* book.classList.add("flipped"); */
-        pag1.classList.add("flipped");
-        pag2.classList.add("flipped");
-        pag1.style.zIndex = 1;
-        pag2.style.zIndex = 2;
-        break;
-      default:
-        throw new Error("error de estado");
-    }
-    btn.style.display = "none";
-    base++;
-  }
+  pag1.classList.add("flipped");
+  pag2.classList.add("flipped");
+  pag1.style.zIndex = 1;
+  pag2.style.zIndex = 2;
+
 }
